469413c2b5f8a44196b528d9f09d7df33ec297d2
Jorge Urquiza <jorgestudio2017@gmail.com>
2026-02-14 01:19:36 -0400

    fix: test should render app header

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MessageService } from 'primeng/api';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -16,6 +17,7 @@ describe('App', () => {
       declarations: [
         AppComponent
       ],
+      providers: [MessageService],
     }).compileComponents();
   });
 
